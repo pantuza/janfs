6a3a48b16fdabf278f7c4311f8b8a2826def2300
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-10-27 02:54:28 -0200

    TCP socket listener to handle client connections. Base structure created

---
+++ daemon/listener.c
@@ -0,0 +1,45 @@
+/**
+ * TCP socket listener
+ */
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+
+#define BUFFER_SIZE 1024
+#define SRV_PORT 5000
+
+
+void fill_values(struct sockaddr_in *addr)
+{
+    bzero(addr, sizeof(*addr));
+    addr->sin_family = AF_INET;
+    addr->sin_addr.s_addr = htonl(INADDR_ANY);
+    addr->sin_port = htonl(SRV_PORT);
+}
+
+void tcp_listener()
+{
+    int sockfd, connfd;
+    struct sockaddr_in srvaddr, cliaddr;
+    socklen_t clilen;
+
+    char clibuff[BUFFER_SIZE];
+
+    sockfd = socket(AF_INET, AF_STREAM, 0);
+    fill_values(&srvaddr);
+    bind(sockfd, (struct sockaddr *) &srvaddr, sizeof(srvaddr));
+
+    listen(sockfd, BUFFER_SIZE);
+
+    clilen = sizeof(cliaddr);
+    connfd = accept(sockfd, (struct sockaddr *) &cliaddr, &clilen);
+
+    while (1) {
+    
+        
+    }
+
+    close(connfd);
+}
