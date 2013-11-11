4b4f627b5d6dcde9fbce4b0a62da248704a4b9e6
Luiz Gustavo <luizgustavops@gmail.com>
2013-11-10 22:24:38 -0200

    Renamed 'janfs.h' and 'janfs.c'. Added debug messages and exit function. Tested for kernel version 3.11.0-rc7+.

--- janfs_fs.c
+++ janfs_fs.c
@@ -8,25 +8,33 @@
 // This file is released under the GPL.
 //
 //-----------------------------------------------------------------------------
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/fs.h>
 #include <linux/backing-dev.h>
 #include <asm/uaccess.h>
 
 #include "socket.h"
-#include "janfs.h"
+#include "janfs_fs.h"
 
 //-----------------------------------------------------------------------------
-static const struct super_operations janfs_ops;
-static const struct inode_operations janfs_dir_inode_operations;
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Luiz Gustavo / Gustavo Pantuza");
 
 static char *remote_addr = "255.255.255.255";
-module_param(remote_addr, charp, 0);
-MODULE_PARM_DESC(remote_addr, "A remote IP address to connect.");
+module_param(remote_addr, charp, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(remote_addr, "A remote IP address to connect");
 
 static int remote_port = 0;
-module_param(remote_port, int, 0);
-MODULE_PARAM_DESC(remote_port, "A remote port to connect.");
+module_param(remote_port, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(remote_port, "A remote port to connect");
 
+
+
+static const struct super_operations janfs_ops;
+static const struct inode_operations janfs_dir_inode_operations;
 
 //-----------------------------------------------------------------------------
 /*
@@ -74,6 +82,23 @@ static struct file_system_type janfs_fstype = {
 struct dentry *janfs_mount(struct file_system_type *fs_type,
 	int flags, const char *dev_name, void *data)
 {
+	int ret = 0;
+
+	printk("Mounting janfs with remote addr=%s and port=%d.\n", remote_addr, remote_port);
+
+	ret = create_client_socket();
+	if (ret) {
+		printk(KERN_ERR "Could not create client socket.\n");
+		return ret;
+	}
+
+   // Connect to specified address
+	ret = connect_server(remote_addr, remote_port);
+	if (ret) {
+		printk(KERN_ERR "Could not connect to remote host.\n");
+      return NULL;
+	}
+
 	return mount_nodev(fs_type, flags, data, janfs_fill_super);
 }
 
@@ -99,25 +124,25 @@ int janfs_fill_super(struct super_block *sb, void *data, int silent)
 /* 
  * Attaches the new filesystem to the kernel.
  */
-static int __init init_janfs(void)
+static int __init janfs_init(void)
 {
-	int ret = 0;
-
-	// Connect to specified address
-	ret = create_client_socket();
-	if (ret) {
-		printk(KERN_ERR "Could not create client socket.");
-		return ret;
-	}
-
-	ret = connect_server(remote_addr, remote_port);
-	if (ret) {
-		printk(KERN_ERR "Could not connect to remote host.");
-	}
+	printk("Registering janfs filesystem.\n");
 
 	return register_filesystem(&janfs_fstype);
 }
 
-module_init(init_janfs)
+/* 
+ * Removes the filesystem from the kernel.
+ */
+static void __exit janfs_exit(void)
+{
+	printk("Removing janfs filesystem.\n");
+
+	unregister_filesystem(&janfs_fstype);
+}
+
+module_init(janfs_init)
+module_exit(janfs_exit)
+
 
 
